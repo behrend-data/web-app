bc03216d89938a9ce878f4e5c82e7a3feaad2a2c
FlemmingBehrend <topfeb@gmail.com>
2021-01-07 22:54:48 +0100

    try new scrollspy

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import About from './sections/about';
 import Home from './sections/home';
 import Contact from './sections/contact';
 // import Cv from './sections/cv';
 import LinkedinLogo from './linkedin-logo.png';
+import ScrollspyNav from 'react-scrollspy-nav';
 
 function App() {
-    const [homeActive, setHomeActive] = useState(true);
-    const [aboutActive, setAboutActive] = useState(false);
-    const [contactActive, setContactActive] = useState(false);
-
     const openMyProfile = () => {
         window.open('https://www.linkedin.com/in/flemming-behrend-405309a');
-    };
-
-    const activate = (name) => {
-        switch (name) {
-            case 'home':
-                setHomeActive(true);
-                setAboutActive(false);
-                setContactActive(false);
-                break;
-            case 'about':
-                setHomeActive(false);
-                setAboutActive(true);
-                setContactActive(false);
-                break;
-            case 'contact':
-                setHomeActive(false);
-                setAboutActive(false);
-                setContactActive(true);
-                break;
-        }
     };
 
     return (
@@ -43,43 +20,35 @@ function App() {
                         <b>BEHREND</b> DATA
                     </div>
                     <div className="col-4">
-                        <ul className="nav navbar">
-                            <li className="nav-item">
-                                <a
-                                    className={`${homeActive ? 'active' : ''} nav-link`}
-                                    href="#home"
-                                    onClick={() => activate('home')}
-                                >
-                                    Home
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    className={`${aboutActive ? 'active' : ''} nav-link`}
-                                    active={aboutActive}
-                                    href="#about"
-                                    onClick={() => activate('about')}
-                                >
-                                    About
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    className={`${contactActive ? 'active' : ''} nav-link`}
-                                    active={contactActive}
-                                    href="#contact"
-                                    onClick={() => activate('contact')}
-                                >
-                                    Contact
-                                </a>
-                            </li>
-                            <img
-                                src={LinkedinLogo}
-                                alt="my linkedin profile"
-                                className="linkedin"
-                                onClick={openMyProfile}
-                            />
-                        </ul>
+                        <ScrollspyNav
+                            scrollTargetIds={['home', 'about', 'contact']}
+                            activeNavClass="active"
+                            scrollDuration="100"
+                        >
+                            <ui className="nav navbar">
+                                <li className="nav-item">
+                                    <a className="active nav-link" href="#home">
+                                        Home
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#about">
+                                        About
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#contact">
+                                        Contact
+                                    </a>
+                                </li>
+                                <img
+                                    src={LinkedinLogo}
+                                    alt="my linkedin profile"
+                                    className="linkedin"
+                                    onClick={openMyProfile}
+                                />
+                            </ui>
+                        </ScrollspyNav>
                     </div>
                 </div>
             </nav>
